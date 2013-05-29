3b49b27ccaf4e8a6024f1c5623d431c274922bb3
George Hartzell <hartzell@alerce.com>
2013-05-29 10:03:28 -0700

    Hook up colors.

--- lib/Log/Dis/Pleasure/Screen.pm
+++ lib/Log/Dis/Pleasure/Screen.pm
@@ -7,7 +7,7 @@ use Moo;
 use Log::Dis::Patchy::Helpers qw(append_newline_callback);
 
 sub _build_ldo_name         {'screen'}
-sub _build_ldo_package_name {'Log::Dispatch::Screen'}
+sub _build_ldo_package_name {'Log::Dispatch::Screen::Color'}
 
 with 'Log::Dis::Patchy::Output';
 
@@ -16,9 +16,17 @@ around _build_ldo_init_args => sub {
     my $args = $self->$orig(@_);
 
     $args->{callbacks} = [ append_newline_callback() ];
-    $args->{max_level} = 'info'
-        if ( grep( /stdout/, @{ $self->_patchy->quiet_fatal } ) );
-    $args->{stderr} = 0;
+    $args->{stderr}    = 0;
+    $args->{color}     = {
+        debug   => { text => 'blue' },
+        info    => { text => 'green', },
+        warning => { text => 'red', },
+        error   => {
+            background => 'white',
+            bold       => 1,
+            text       => 'red',
+        },
+    };
 
     return $args;
 };
